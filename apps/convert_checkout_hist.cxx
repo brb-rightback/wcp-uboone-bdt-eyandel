42db4b356e85bea906b9854c44640e7948a94d1b
Benjamin Bogart <bbogart@uboonegpvm03.fnal.gov>
2025-11-11 08:26:18 -0600

    Cleanup

--- apps/convert_checkout_hist.cxx
+++ apps/convert_checkout_hist.cxx
@@ -24,7 +24,6 @@ int main( int argc, char** argv )
 
   TString input_filename = argv[1];
   TString out_filename = argv[2];
-  std::cout<<out_filename<<std::endl;
   bool flag_data = true;
 
   bool flag_osc = false;
@@ -118,11 +117,10 @@ int main( int argc, char** argv )
     TString add_cut = std::get<6>(*it);
     TString weight = std::get<7>(*it);
 
-        std::cout << std::get<0>( *it)  << " " << std::get<1>(*it) << " " << std::get<4>(*it) << " " << std::get<5>(*it) << " " << std::get<6>(*it) << " " << std::get<7>(*it) << std::endl;
+    //    std::cout << std::get<0>( *it)  << " " << std::get<1>(*it) << " " << std::get<4>(*it) << " " << std::get<5>(*it) << " " << std::get<6>(*it) << " " << std::get<7>(*it) << std::endl;
     htemp = new TH1F(histoname, histoname, nbin, llimit, hlimit);
     map_histoname_hist[histoname] = htemp;
   }
-    std::cout <<map_histoname_hist.size() <<std::endl;
 
   std::vector< std::tuple<TString,  int, float, float, TString, TString, TString, TString > > histo_infos_err2 = cov.get_histograms(input_filename,1);
   std::copy(histo_infos_err2.begin(), histo_infos_err2.end(), std::back_inserter(all_histo_infos));
@@ -453,8 +451,6 @@ int main( int argc, char** argv )
       TString add_cut = std::get<6>(*it);
       TString weight = std::get<7>(*it);
 
-      if(i<20) std::cout << std::get<0>( *it)  << " " << std::get<1>(*it) << " " << std::get<4>(*it) << " " << std::get<5>(*it) << " " << std::get<6>(*it) << " " << std::get<7>(*it) << std::endl;
-
       htemp = map_histoname_hist[histoname];
       // get kinematics variable ...
       double val = get_kine_var(kine, eval, pfeval, tagger, flag_data, var_name, space, pandora, lantern);
@@ -491,11 +487,8 @@ int main( int argc, char** argv )
   T->Branch("pot",&total_pot,"pot/D");
   T->Fill();
 
-  std::cout <<map_histoname_hist.size() <<std::endl;
-
-  std::cout<<out_filename<<std::endl;
   for (auto it = map_histoname_hist.begin(); it!= map_histoname_hist.end(); it++){
-    std::cout<<"DEBUG: "<<it->first<<" "<<it->second->GetName()<<" "<<it->second->GetSum()<<"\n";
+    //std::cout<<"DEBUG: "<<it->first<<" "<<it->second->GetName()<<" "<<it->second->GetSum()<<"\n";
     it->second->SetDirectory(file1);
   }
 
